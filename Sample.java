7640190484a20f5924976b2940f3216e985dff9e
abhishekawatiabhi-oss <abhishekawatiabhi@gmail.com>
2026-09-01 14:00:11 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins");
-        System.out.println("automaticall triggering")
+        System.out.println("automaticall triggering");
     }
 }
